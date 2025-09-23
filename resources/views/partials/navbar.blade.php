60cf62a8086c2454faf7f64522b80d5be807f09e
Arya Nugraha <aryanugraha0464@gmail.com>
2025-09-24 02:42:05 +0700

    fixing css error

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,41 +1,43 @@
-<nav class="bg-white border-b border-gray-200">
-    <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-20">
-            <div class="flex items-center space-x-8">
-                <a href="/" class="flex-shrink-0">
-                    {{-- Ganti baris ini dengan komponen SVG logo Anda jika menggunakan itu --}}
-                    <img class="h-8 w-auto" src="{{ asset('assets/svg/logo-luarsekolah.svg') }}" alt="Logo Luarsekolah">
+<nav class="bg-white h-[104px] flex items-center shadow-[0px_8px_16px_rgba(31,39,56,0.08)]">
+    
+    <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
+        <div class="flex items-center justify-between gap-4">
+
+            <div class="flex items-center gap-8">
+                <a href="/">
+                    <img class="w-[149px] h-[75px] object-contain" src="{{ asset('assets/svg/logo-luarsekolah.svg') }}" alt="Logo Luarsekolah">
                 </a>
 
                 <div class="hidden md:flex">
-                    <button class="flex items-center text-gray-700 font-semibold hover:text-green-600">
+                    <button class="flex items-center gap-1.5 text-black text-lg font-normal">
                         <span>Program</span>
-                        <svg class="ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                         </svg>
                     </button>
                 </div>
             </div>
 
-            <div class="flex-1 max-w-lg mx-8 hidden lg:flex">
+            <div class="flex-1 max-w-[621px] hidden lg:flex">
                 <div class="relative w-full">
                     <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                        <svg class="w-6 h-6 text-[#9BA1A9]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                         </svg>
                     </div>
-                    <input type="text" placeholder="Cari Kelas yang ingin kamu pelajari" class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md focus:ring-green-500 focus:border-green-500">
+                    <input type="text" placeholder="Cari Kelas yang ingin kamu pelajari" class="w-full h-11 pl-11 pr-4 border border-[#9BA1A9] rounded-md text-sm placeholder:text-[#9BA1A9] focus:ring-green-500 focus:border-green-500">
                 </div>
             </div>
 
-            <div class="flex items-center space-x-4">
-                <a href="#" class="px-6 py-2 border border-gray-300 text-gray-700 font-semibold rounded-md hover:bg-gray-50">
+            <div class="hidden md:flex items-center gap-4">
+                <a href="#" class="w-[139px] h-11 flex justify-center items-center border border-[#9BA1A9] rounded-md text-[#1F2738] text-sm font-semibold hover:bg-gray-50">
                     Masuk
                 </a>
-                <a href="#" class="px-6 py-2 bg-teal-600 text-white font-semibold rounded-md hover:bg-teal-700">
+                <a href="#" class="w-[139px] h-11 flex justify-center items-center bg-[#005545] rounded-md text-[#E6E7E8] text-sm font-semibold hover:bg-[#004437]">
                     Daftar
                 </a>
             </div>
+
         </div>
     </div>
 </nav>
