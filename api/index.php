29542f358110e049089ed00b96cd101776fc57dc
Arya Nugraha <aryanugraha0464@gmail.com>
2025-09-24 03:05:41 +0700

    fixing css error

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
-<?php  
+<?php
 
-require __DIR__ . "/../public/index.php";
+// File: api/index.php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+// Baris ini adalah satu-satunya yang penting.
+// Dia akan memanggil file index.php utama dari Laravel Anda.
+require __DIR__ . '/../public/index.php';
